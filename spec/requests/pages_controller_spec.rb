e2ee11a679d697b90453d225ca02029866c28153
Isaac Barbosa <izass1312@gmail.com>
2023-04-10 17:08:48 -0300

    Implement spec to PagesController

--- spec/requests/pages_controller_spec.rb
+++ spec/requests/pages_controller_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "PagesControllers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe PagesController, type: :request do
+  describe 'GET /' do
+    it 'renders home page' do
+      get root_url
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
   end
 end
